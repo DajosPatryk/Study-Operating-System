9063462df39abfe27f4326ebd4d728b0ee02d59c
DajosPatryk <patryk00.business@gmail.com>
2024-05-05 18:43:02 +0200

    I dont fucking know.

--- system_core/src/io/PrintStream.cc
+++ system_core/src/io/PrintStream.cc
@@ -24,178 +24,199 @@ void PrintStream::println() {
 }
 
 void PrintStream::print(int x, int base) {
+    //0 can be immediately be solved
+    if(x ==0){
+        char c = x +'0';
 
-    // Solving 0 immediately
-    if (x == 0) {
-        char c = x + '0';
-        if (base == 2) {
+        if(base ==2){
             print("0b");
             print(c);
-        } else if (base == 16) {
+        }else if(base == 16){
             print("0x");
             print(c);
-        } else print(c);
-        return;
-    }
-
-    if (base != 2 && base != 10 && base != 16) {
-        print("Error: Unknown digit base.");
-        return;
-    }
-
-    // Inverts x, saves information
-    bool negativ = false;
-    if (x < 0) {
-        negativ = true;
-        x = -(x);
-    }
-
-    /**
-     * Source: https://www.geeksforgeeks.org/how-to-convert-given-number-to-a-character-array/
-     *
-     * No string as object in C, only C-Style strings, need to do run algorithm
-     * Counts digits in number x
-     */
-    int m = x;
-    int digit = 0;
-    while (m) {
-        digit++;    // Increments number of digits
-        m /= base;  // Truncates the last digit from the number
-    }
-
-    char arr[digit];    // Result
-    char arr1[digit];   // Duplicate, important as array will be reversed for final result
-
-    // Separating integer into digits and accommodating to char arr
-    int index = 0;
-    while (x) {
-        // Separate last digit from the number and add ASCII val of char, '0' is 48
-        if (x % base < 10) {
-            arr1[++index] = x % base + '0';
+        }else{
+            print(c);
         }
-        else {
-            int result = x % base;
-            switch (result) {
-                case 10: arr1[++index] = 'A'; break;
-                case 11: arr1[++index] = 'B'; break;
-                case 12: arr1[++index] = 'C'; break;
-                case 13: arr1[++index] = 'D'; break;
-                case 14: arr1[++index] = 'E'; break;
-                case 15: arr1[++index] = 'F';
+
+    }else{
+        //only accept numbers with acceptable base of 2, 10 or 16
+        if(base == 2 || base ==10 || base == 16){
+            //invert x to positive value if negative but save information it was negative
+            bool negativ = false;
+            if(x<0){
+                negativ = true;
+                x = -(x);
             }
+
+            //https://www.geeksforgeeks.org/how-to-convert-given-number-to-a-character-array/
+            //no string as object in c, need to do algorithm "per hand"
+            // Count digits in number x
+            int m = x;
+            int digit = 0;
+            while (m) {
+
+                // Increment number of digits
+                digit++;
+
+                // Truncate the last
+                // digit from the number
+                m /= base;
+            }
+
+            // Declare char array for result
+            char arr[digit];
+            // Declare duplicate char array
+            char arr1[digit];
+            //those are important because we will reverse array for final result
+
+            // Separating integer into digits and
+            // accommodate it to character array
+            int index = 0;
+            while (x) {
+                // Separate last digit from
+                // the number and add ASCII
+                // value of character '0' is 48
+                if(x%base <10){
+                    arr1[++index] = x % base + '0';
+                }else{
+                    int result = x%base;
+                    switch (result){
+                        case 10: arr1[++index] = 'A';break;
+                        case 11: arr1[++index] = 'B';break;
+                        case 12: arr1[++index] = 'C';break;
+                        case 13: arr1[++index] = 'D';break;
+                        case 14: arr1[++index] = 'E';break;
+                        case 15: arr1[++index] = 'F';
+                    }
+                }
+                // Truncate the last
+                // digit from the number
+                x /= base;
+            }
+
+            // Reverse the array for result
+            int i;
+            for (i = 0; i < index; i++) {
+                arr[i] = arr1[index - i];
+            }
+
+            // Char array truncate by null
+            arr[i] = '\0';
+
+            if(negativ==false){
+                if(base == 2){
+                    print("0b");
+                    print(arr);
+                }else if(base ==16){
+                    print("0x");
+                    print(arr);
+                }else{
+                    print(arr);
+                }
+
+
+            }else{
+                if(base == 2){
+                    print('-');
+                    print("0b");
+                    print(arr);
+                }else if(base ==16){
+                    print('-');
+                    print("0x");
+                    print(arr);
+                }else{
+                    print('-');
+                    print(arr);
+                }
+
+            }
+        }else{
+            print("falsche Basis");
         }
-
-        // Truncates the last digit from the number
-        x /= base;
-    }
-
-    // Reverses the array for result
-    int i;
-    for (i = 0; i < index; i++) { arr[i] = arr1[index - i]; }
-    arr[i] = '\0';  // Char array truncates by null
-
-    if (!negativ) {
-        if (base == 2) {
-            print("0b");
-            print(arr);
-        } else if (base == 16) {
-            print("0x");
-            print(arr);
-        } else print(arr);
-
-
-
-    } else {
-        if (base == 2) {
-            print('-');
-            print("0b");
-            print(arr);
-        } else if (base == 16) {
-            print('-');
-            print("0x");
-            print(arr);
-        } else {
-            print('-');
-            print(arr);
-        }
-
     }
 }
 
 
 void PrintStream::print(unsigned x, int base) {
-
-    // Solving 0 immediately
-    if (x == 0) {
-        char c = x + '0';
-        if (base == 2) {
+    //similar to previous one, don't need to care for negative
+    //but needs separate function as unsigned int can store bigger number
+    if(x ==0){
+        char c = x +'0';
+        if(base ==2){
             print("0b");
             print(c);
-        } else if (base == 16) {
+        }else if(base == 16){
             print("0x");
             print(c);
-        } else print(c);
-        return;
-    }
-
-    if (base != 2 && base != 10 && base != 16) {
-        print("Error: Unknown digit base.");
-        return;
-    }
-
-    /**
-     * Source: https://www.geeksforgeeks.org/how-to-convert-given-number-to-a-character-array/
-     *
-     * No string as object in C, only C-Style strings, need to do run algorithm
-     * Counts digits in number x
-     */
-    unsigned int m = x;
-    unsigned int digit = 0;
-    while (m) {
-        digit++;    // Increments number of digits
-        m /= base;  // Truncates the last digit from the number
-    }
-
-    char arr[digit];    // Result
-    char arr1[digit];   // Duplicate, important as array will be reversed for final result
-
-    // Separating integer into digits and accommodating to char arr
-    unsigned int index = 0;
-    while (x) {
-        // Separate last digit from the number and add ASCII val of char, '0' is 48
-        if (x % base < 10) {
-            arr1[++index] = x % base + '0';
-        } else {
-            int result = x % base;
-            switch (result) {
-                case 10: arr1[++index] = 'A'; break;
-                case 11: arr1[++index] = 'B'; break;
-                case 12: arr1[++index] = 'C'; break;
-                case 13: arr1[++index] = 'D'; break;
-                case 14: arr1[++index] = 'E'; break;
-                case 15: arr1[++index] = 'F';
-            }
+        }else{
+            print(c);
         }
-        // Truncates the last digit from the number
-        x /= base;
-    }
+    }else{
 
-    // Reverses the array for result
-    int i;
-    for (i = 0; i < index; i++) { arr[i] = arr1[index - i]; }
-    arr[i] = '\0';  // Char array truncates by null
+        if(base == 2 || base ==10 || base == 16){
+            // Count digits in number x
+            unsigned int m = x;
+            unsigned int digit = 0;
+            while (m) {
+
+                // Increment number of digits
+                digit++;
+                // Truncate the last
+                // digit from the number
+                m /= base;
+            }
+
+            // Declare char array for result
+            char arr[digit];
+            // Declare duplicate char array
+            char arr1[digit];
+
+            // Separating integer into digits and
+            // accommodate it to character array
+            unsigned int index = 0;
+            while (x) {
+                // Separate last digit from
+                // the number and add ASCII
+                // value of character '0' is 48
+                if(x%base <10){
+                    arr1[++index] = x % base + '0';
+                }else{
+                    int result = x%base;
+
+                    switch (result){
+                        case 10: arr1[++index] = 'A';break;
+                        case 11: arr1[++index] = 'B';break;
+                        case 12: arr1[++index] = 'C';break;
+                        case 13: arr1[++index] = 'D';break;
+                        case 14: arr1[++index] = 'E';break;
+                        case 15: arr1[++index] = 'F';
+                    }
+                }
+                // Truncate the last
+                // digit from the number
+                x /= base;
+            }
+            // Reverse the array for result
+            unsigned int i;
+            for (i = 0; i < index; i++) {
+                arr[i] = arr1[index - i];
+            }
+            // Char array truncate by null
+            arr[i] = '\0';
 
 
-    if (base == 2) {
-        print("0b");
-        print(arr);
-    } else if (base == 16) {
-        print("0x");
-        print(arr);
-    } else {
-        print(arr);
-    }
-
+            if(base == 2){
+                print("0b");
+                print(arr);
+            }else if(base ==16){
+                print("0x");
+                print(arr);
+            }else{
+                print(arr);
+            }
+        }else{
+            print("falsche Basis");
+        }}
 }
 
 
