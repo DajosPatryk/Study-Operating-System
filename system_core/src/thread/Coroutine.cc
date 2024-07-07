9f75114d57d5188c42aff3606f3a9422a180b4c5
Janusz Manczak <janusz123manczak@gmail.com>
2024-07-07 20:44:52 +0200

    problem solved in coroutine stack initialization, wasn't properly saved

--- system_core/src/thread/Coroutine.cc
+++ system_core/src/thread/Coroutine.cc
@@ -20,26 +20,29 @@ void Coroutine::startup(Coroutine* obj) {
 // Bei nullptr es geht auf stack von main, dadurch kein eigenen gebraucht
 void Coroutine::setup(void* tos) {
     // If tos is not nullptr, initialize a new stack from the given stack pointer.
-	if (tos != 0) {
-		Stack* stackTos = (Stack*)tos;
+	if (!(tos == 0x0)) {
+		this->sp = tos;
+		Stack stackTos = Stack();
 
 		// stack mit 0 initialisieren, vermeidet Datenanderung.
         // Initialize stack registers to 0 to prevent data corruption.
-		stackTos->edi = 0;
-		stackTos->esi = 0;
-		stackTos->ebx = 0;
-		stackTos->ebp = 0;
+		stackTos.edi = 0;
+		stackTos.esi = 0;
+		stackTos.ebx = 0;
+		stackTos.ebp = 0;
 
 		// Methode coroutine() mit startup() Methode initialisieren,
 		// da dieser die parameter Instanz benoetigt.
         // Set up the coroutine function pointer to the 'startup' method,
-		stackTos->coroutine = &startup;
-		stackTos->ret = 0;
+		stackTos.coroutine = &Coroutine::startup;
+		stackTos.ret = 0;
 
 		// arg, ein Instanz der Klasse Coroutine.
-		stackTos->arg = this;
-		this->sp = stackTos;
-		stackTos->arg = this;   // Store a pointer to this coroutine instance in 'arg' for use by 'startup'.
-		this->sp = stackTos;    // Save the pointer to the initialized stack in the coroutine's stack pointer (sp).
-	}
+		stackTos.arg = this;
+		//jetzt so vorbereitete Stack speichern
+		Stack *st = (Stack *)tos;
+		st--;
+		this->sp = st;
+		*(st) = stackTos;
+		}
 }
